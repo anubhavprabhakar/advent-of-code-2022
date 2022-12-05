8c2ca26781f592bf04db2b37b62c302d735e60c3
anubhavprabhakar <anu.devjob@gmail.com>
2022-12-06 03:36:45 +0530

    dec 5 part 1&2, with explaination.py

---
+++ dec 5/dec 5 part 2.py
@@ -0,0 +1,73 @@
+stacks=open('file1.txt', 'r')
+inst=open('file2.txt', 'r')
+
+ins = inst.read()
+s = stacks.read()
+
+
+#storing in list format
+stc = s.split('\n')
+
+
+#for getting number of stacks
+nums=stc.pop()
+numOfStc=int(nums[-2])
+
+arr=[]
+d={}
+
+#accessing each element from the stacks(in the rocket ship) to put into particular list
+
+for i in range(0, len(stc)):
+    element=0   #to know which stack the following element belongs to
+    for j in range(1, len(stc[i]), 4):
+        element+=1
+        if(stc[i][j]!=' '):
+            
+            if element in d.keys():
+                d[element].append(stc[i][j])
+                
+            else:
+                dic={element: []}
+                d.update(dic)
+                d[element].append(stc[i][j])
+                
+
+
+#to reverse the list of stacks, so that popping and appending operations give correct representation of items moved
+for i in range(1, numOfStc+1):
+    d[i].reverse()
+
+
+#accessing instructions
+ins=ins.split('\n')
+
+#print(ins)
+
+for i in range(0, len(ins)):
+    instruction = ins[i].split()
+
+    #so the instruction is in the form of 'move X' 'from X' 'to X', hence naming the variables    
+    move = int(instruction[1])
+    _from = int(instruction[3])
+    to = int(instruction[5])
+
+    moving=[]
+    #print("moving(before):", moving)
+    for j in range(0, move):  #moving 'move' number of times    
+        moving.append(d[_from].pop())        
+
+    moving.reverse()
+    #print("moving:", moving)
+    for k in range(0, len(moving)):
+        destStack=d[to].append(moving[k])#placing that element into the 'to' list it asked(the instruction asked)
+    #print(d)
+
+
+#printing solution on screen
+for i in range(1, numOfStc+1):
+    print(d[i][-1], end="")
+
+
+
+        
