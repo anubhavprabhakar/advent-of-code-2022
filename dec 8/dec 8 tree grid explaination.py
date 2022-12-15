84fe742412a070bbac0a9c503246448c55c5494e
anubhavprabhakar <anu.devjob@gmail.com>
2022-12-15 17:08:35 +0530

    logic to show visible trees

--- dec 8/dec 8 tree grid explaination.py
+++ dec 8/dec 8 tree grid explaination.py
@@ -16,24 +16,76 @@ def addToVisible(location):
     if(location not in vis_list):
         vis_list.append(location)
 
-down_max=0
-right_max=0
 
 
 def up():
-    for i in range(1, 
-        temp=
+    for i in range(1, noOfRows-1):
+        print('Row:', i, end=', ')
+        temp = grid[0][i]
+        
+        for j in range(1, noOfColumns-1):
+            if(grid[j][i]>temp):
+                print('temp', temp, end=', ')
+                temp = grid[j][i]
+                print('loc: grid[',str(j)+str(i), '] tree: ', grid[j][i])
+        else:
+            print()
+                
+def down():
+    for i in range(1, noOfRows-1):
+        print('Row:', i, end=', ')
+        temp = grid[noOfColumns-1][i]
+        
+        for j in range(noOfColumns-2, 0, -1):
+            if(grid[j][i]>temp):
+                print('temp', temp, end=', ')
+                temp = grid[j][i]
+                print('loc: grid[',str(j)+str(i), '] tree: ', grid[j][i])
+        else:
+            print()
+            
+def left():
+    for i in range(1, noOfColumns-1):
+        print('Col:', i, end=', ')
+        temp = grid[i][0]
+        
+        for j in range(1, noOfRows-1):
+            if(grid[i][j]>temp):
+                print('temp', temp, end=', ')
+                temp = grid[i][j]
+                print('loc: grid[',str(i)+str(j), '] tree: ', grid[i][j])
+        else:
+            print()
+
+
+def right():
+    for i in range(1, noOfColumns-1):
+        print('Col:', i, end=', ')
+        temp = grid[i][noOfRows-1]
+        
+        for j in range(noOfRows-2, 0, -1):
+            if(grid[i][j]>temp):
+                print('temp', temp, end=', ')
+                temp = grid[i][j]
+                print('loc: grid[',str(i)+str(j), '] tree: ', grid[i][j])
+        else:
+            print()
 
 
 
 
 
 
-def comparison():
-    up()
-    down()
-    left()
-    right()
+print('\nup()')
+up()
+print('\n\ndown()')
+down()
+print('\n\nleft()')
+left()
+print('\n\nright()')
+right()
+
+
 
 
 
